2c6fe5c033ccac5a3a30278a0a1a639357d2a933
Jonesst07 <100109195+Jonesst07@users.noreply.github.com>
2022-10-04 17:54:44 -0500

    Add files via upload

---
+++ Inflation.py
@@ -0,0 +1,44 @@
+# Heading
+
+# Sherena Jones
+# CSCI 130
+# Due date -- 10/04/2022
+
+# Inflation.py
+# This program will calculate inflation 
+
+def main():
+
+    # Heading - Describes the purpose of the program to the user.
+    print("This program will determine a rate of inflation. ")
+    print("You will be required to enter 3 numbers: ")
+    print("1. a previous price of an item ")
+    print("2. a current price of that same item ")
+    print("3. the number of weeks between prices ")
+    
+    print()
+
+
+    # Input
+    previous_price = float(input('Enter previous price: '))
+    print()
+    current_price = float(input('Enter current price: '))
+    print()
+    number_of_weeks = int(input('Enter the number of weeks: '))
+    print()
+
+    # Process
+    price_difference = current_price - previous_price
+
+
+
+    # Output
+
+
+
+
+
+
+
+
+main()
