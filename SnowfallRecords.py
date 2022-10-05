1e13abc81b024824fc5e0d735e956fb23ca48879
Jonesst07 <100109195+Jonesst07@users.noreply.github.com>
2022-10-05 17:23:09 -0500

    Add files via upload

---
+++ SnowfallRecords.py
@@ -0,0 +1,55 @@
+# Heading
+
+# Sherena Jones
+# CSCI-130
+
+# Program: SnowfallRecords.py
+# Due:  9/23/2022
+
+# Average Snowfall
+
+def main():
+    
+    print()
+    print('Winter of 2022-2023 - 3 Snowfalls')
+    print()
+    print('This program will ask the user for information about 3 snowfalls.')
+    print('It will calculate statistics about them and print a report at the end.')
+    print()
+    
+    # Input
+    snowfall_1 = eval(input('How many inches in the first snowfall? '))
+    
+    snowfall_2 = eval(input('How many inches in the second snowfall? '))
+    
+    snowfall_3 = eval(input('How many inches in the third snowfall? '))
+
+    
+    
+    
+    
+    
+    
+    average = (snowfall_1 + snowfall_2 + snowfall_3) /2
+
+    
+    
+    
+    
+    
+
+    # Step 2: Figure out equivalent rain amount (snow inches divided by 13)
+    # for the snow total and save in a new variable
+    
+
+    # Step 3: Figure out average snowfall per storm and save in a new variable
+    
+
+    # OUTPUT (12 total lines, not including blank line before report)
+    # Report all values that were entered or calculated in specific format
+    # (Before report lines, "print" an empty line)
+
+
+    
+
+main()
